29026eb7f7f3c54e20ea563fe3b5bffa9e28b4cf
Maryam Ijaz <maryamijaztabassum@gmail.com>
2026-08-11 20:19:53 +0500

    Create lists.py

---
+++ Module-1-Introduction-to-AI-Python/Python/lists.py
@@ -0,0 +1,10 @@
+# Python Lists
+
+skills = ["Python", "AI", "Machine Learning", "SQL"]
+
+print("My skills:", skills)
+print("First skill:", skills[0])
+
+skills.append("Deep Learning")
+
+print("Updated skills:", skills)
